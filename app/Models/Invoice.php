fb610855382af8e0252daa95297e65e56f2716e3
wawanneutron <wawanneutron1331@gmail.com>
2021-04-21 21:52:56 +0700

    add Model & Mgration

---
+++ app/Models/Invoice.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    use HasFactory;
+
+    protected $fillabel = [
+        'invoice', 'customer_id', 'courier', 'service', 'cost_courier', 'weight',
+        'name', 'phone', 'province', 'city', 'address', 'status', 'snap_token', 'grand_total'
+    ];
+}
