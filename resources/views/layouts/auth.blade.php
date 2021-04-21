da4d83ec4ccae73226a827b8945b6476e900eab3
wawanneutron <wawanneutron1331@gmail.com>
2021-04-22 06:31:57 +0700

    layouts dashboard-view auth login

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>{{ $title ?? config('app.name') }} - Admin</title>
+
+   @include('includes.dashboard-style')
+</head>
+
+<body style="background-color: #e2e8f0;">
+
+    @yield('content')
+
+  {{-- script --}}
+   @include('includes.dashboard-script')
+</body>
+</html>
